8b8ced08e465cb6427474849ca3ea85a2cff560b
Heber Lazo <heberlazo@gmail.com>
2023-03-04 17:42:49 -0500

    Added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+password = '123456'
+
+2.times.each { |index| User.create(email: "user_#{index}@gmail.com", password: password, password_confirmation: password) }
+
+users = User.all
+
+users.each do |user|
+  3.times do
+    Note.create(
+      user_id: user.id,
+      title: Faker::Games::LeagueOfLegends.champion,
+      body: Faker::Lorem.sentences(number: [8,16,20,28].sample).join(''),
+      note_date: Date.today + [2,4,8].sample.to_d)
+  end
+end
+
