8c3f6365038966a8473a46d382c06978d87755f2
Heber Lazo <heberlazo@gmail.com>
2023-03-04 17:02:54 -0500

    Added note validations

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,20 @@
 class Note < ApplicationRecord
   belongs_to :user
+  validates_presence_of :title, :body
+  validate :date_presence?
+  validate :valid_date?, unless: :date_presence?
+
+
+  def date_presence?
+    return if self.note_date.present?
+
+    errors.add(:note_date, "can't be blank")
+  end
+
+
+  def valid_date?
+    return unless Time.now > self.note_date
+
+    errors.add(:date, "can't be in the past")
+  end
 end
