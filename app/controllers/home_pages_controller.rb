faa8a9163b459ec550242ec6355a55d330290a49
Heber Lazo <heberlazo@gmail.com>
2023-03-05 18:58:21 -0500

    Added Rubocop and corrected controller

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class HomePagesController < ApplicationController
   def index
     @welcome = 'Welcome to Proyecto Notas'
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,18 +1,18 @@
+# frozen_string_literal: true
+
 class NotesController < ApplicationController
   include ActionView::RecordIdentifier
   before_action :authenticate_user!
-  before_action :set_note, only: [:show, :update, :edit]
+  before_action :set_note, only: %i[show update edit]
   before_action :selected_params, only: [:index]
   def index
     @note = Note.new
     @notes = @selected_params.blank? ? current_user.notes : current_user.finder(@selected_params)
   end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @note = current_user.notes.build(note_params)
@@ -20,21 +20,22 @@ class NotesController < ApplicationController
     if @note.save
       streams = []
       streams << turbo_stream.prepend('notes', partial: 'notes/note', locals: { note: @note })
-      streams << turbo_stream.replace('note_form', partial: 'notes/form', locals: { note: Note.new } )
+      streams << turbo_stream.replace('note_form', partial: 'notes/form', locals: { note: Note.new })
       render turbo_stream: streams
     else
-      render turbo_stream: turbo_stream.replace('error_message', partial: "shared/error_message", locals: { message: @note.errors.full_messages.to_sentence })
+      render turbo_stream: turbo_stream.replace('error_message', partial: 'shared/error_message',
+                                                                 locals: { message: @note.errors.full_messages.to_sentence })
     end
-
   end
 
   def update
     streams = []
     if @note.update(note_params)
-    streams << turbo_stream.replace('edit_card', partial: 'full_note', locals: { note: @note } )
-    streams << turbo_stream.replace("#{dom_id @note}", partial: 'note', locals: { note: @note } )
-  else
-    streams << turbo_stream.replace('error_message', partial: 'shared/error_message', locals: { message: @note.errors.full_messages.to_sentence } )
+      streams << turbo_stream.replace('edit_card', partial: 'full_note', locals: { note: @note })
+      streams << turbo_stream.replace(dom_id(@note).to_s, partial: 'note', locals: { note: @note })
+    else
+      streams << turbo_stream.replace('error_message', partial: 'shared/error_message',
+                                                       locals: { message: @note.errors.full_messages.to_sentence })
     end
 
     render turbo_stream: streams
