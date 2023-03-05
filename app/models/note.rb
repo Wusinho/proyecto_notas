d03f04417494c39978d3867d917f2cfc39c6c580
Heber Lazo <heberlazo@gmail.com>
2023-03-05 11:17:17 -0500

    Added validation and removed unnecessary code

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,13 +1,13 @@
 class Note < ApplicationRecord
   belongs_to :user
   validates_presence_of :title, :body, :note_date
+  validates_uniqueness_of :title
   validate :valid_date?, unless: :date_presence?
 
 
   def date_presence?
     self.note_date.blank?
   end
-
 
   def valid_date?
     return unless Date.today > self.note_date
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'home_pages/index'
   root 'home_pages#index'
   resources :notes
 
