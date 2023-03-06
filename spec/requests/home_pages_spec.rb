b6cb56d2bf7f04f0aafd487c9d9b618f8f494aa2
Heber Lazo <heberlazo@gmail.com>
2023-03-06 09:43:19 -0500

    refactor home_page tests

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -1,33 +1,35 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe HomePagesController, type: :request do
   let!(:user) { create(:user) }
-  let!(:notes) { create_list(:note, 5, user: user) }
+  let!(:notes) { create_list(:note, 5, user:) }
 
-  describe "GET #index" do
-    it "renders a successful response" do
+  describe 'GET #index' do
+    it 'renders a successful response' do
       get home_pages_path
       expect(response).to have_http_status(:success)
+      expect(Note.count).to equal(5)
     end
   end
 
-  describe "GET #show" do
-    let(:note) { create(:note, user: user) }
+  describe 'GET #show' do
 
-    it "renders a successful response" do
-      get home_page_path(note)
+    it 'renders a successful response' do
+      get home_page_path(notes[0])
       expect(response).to have_http_status(:success)
     end
 
-    it "displays the requested note" do
-      get home_page_path(note)
-      expect(response.body).to include(note.title)
+    it 'displays the requested note' do
+      get home_page_path(notes[0])
+      expect(response.body).to include(notes[0].title)
     end
 
-    it "does not display other notes" do
-      other_note = create(:note, user: user)
-      get home_page_path(note)
-      expect(response.body).to include(note.title)
+    it 'does not display other notes' do
+      other_note = create(:note, user:)
+      get home_page_path(notes[0])
+      expect(response.body).to include(notes[0].title)
       expect(response.body).not_to include(other_note.title)
     end
   end
