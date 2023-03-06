718d61709181946c5e957e4f23f247601c9bc100
Heber Lazo <heberlazo@gmail.com>
2023-03-06 11:10:26 -0500

    Added test to home_page

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -11,25 +11,30 @@ RSpec.describe HomePagesController, type: :request do
       get home_pages_path
       expect(response).to have_http_status(:success)
       expect(Note.count).to equal(5)
+      notes.each do |note|
+        expect(response.body).to include(note.title)
+        expect(response.body).to include(note.body)
+        expect(response.body).to include(note.note_date)
+      end
     end
   end
 
   describe 'GET #show' do
 
     it 'renders a successful response' do
-      get home_page_path(notes[0])
+      get home_page_path(notes.first)
       expect(response).to have_http_status(:success)
     end
 
     it 'displays the requested note' do
-      get home_page_path(notes[0])
-      expect(response.body).to include(notes[0].title)
+      get home_page_path(notes.first)
+      expect(response.body).to include(notes.first.title)
     end
 
     it 'does not display other notes' do
       other_note = create(:note, user:)
-      get home_page_path(notes[0])
-      expect(response.body).to include(notes[0].title)
+      get home_page_path(notes.first)
+      expect(response.body).to include(notes.first.title)
       expect(response.body).not_to include(other_note.title)
     end
   end
