e0ed9afbf1a33d7816dc1dd1e517cb9a43d77878
Heber Lazo <heberlazo@gmail.com>
2023-03-06 19:52:09 -0500

    refactor filterable

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -4,12 +4,16 @@ module Filterable
 
   module ClassMethods
     def filter(filtering_params, user = nil)
-      results = self.where(nil)
-      results = results.where(user: user) if self.method_defined?(:user)
-      filtering_params.each do |key, value|
-        results = results.public_send("filter_#{key}", value) if value.present?
+      results = where(nil)
+      results = results.where(user:) if method_defined?(:user)
+
+      key = filtering_params.keys.first
+      value = filtering_params.values.first
+      if value.present?
+        results.public_send("filter_#{key}", value)
+      else
+        results.order(created_at: :desc)
       end
-      results
     end
   end
 end
