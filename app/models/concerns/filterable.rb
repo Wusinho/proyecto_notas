195d833404ecd0ddcbca1ad8ac13435c3b991054
Heber Lazo <heberlazo@gmail.com>
2023-03-06 21:41:00 -0500

    refactor filterable

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -6,13 +6,11 @@ module Filterable
     def filter(filtering_params, user = nil)
       results = where(nil)
       results = results.where(user:) if method_defined?(:user) && user
-      key = filtering_params.keys.first
+      return results.order(created_at: :desc) if filtering_params.blank?
+
       value = filtering_params.values.first
-      if value.present?
-        results.public_send("filter_#{key}", value)
-      else
-        results.order(created_at: :desc)
-      end
+      key = filtering_params.keys.first
+      results.public_send("filter_#{key}", value) if value.present?
     end
   end
 end
